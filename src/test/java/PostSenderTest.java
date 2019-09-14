b834520709adaaf76c482fe237ebca6feee60597
dasAbhijit <abhijit18aug@gmail.com>
2019-09-14 16:59:47 +0530

    change folder structure of tests

---
+++ src/test/java/PostSenderTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostSenderTest {
+
+    Post post;
+    Address firstAddress;
+    Stamp stamp;
+    PostSender postSender;
+    List<PostOffice> postOffices;
+
+    @Before
+    public void setUp() {
+        firstAddress = new Address(1, "first Line", "WestBengal", "Kolkata", 700032);
+        stamp = new Stamp(1, Type.NATIONAL, 5);
+        post = new Post(1, firstAddress, firstAddress, "Hi", "How are you?", stamp, firstAddress, firstAddress);
+        postSender = new PostSender();
+        postOffices = new ArrayList<PostOffice>();
+        postOffices.add(new PostOffice(firstAddress));
+    }
+
+    @Test
+    public void shouldBeAbleToSendPost() {
+        postSender.send(post, postOffices);
+    }
+}
